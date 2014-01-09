c7f503eadf053e7ca32a072cc35bc0083c40bd9e
legend0702 <qwepoidjdj@hotmail.com>
2014-01-10 04:14:41 +0800

    change hello
    Signed-off-by: legend0702 <qwepoidjdj@hotmail.com>

--- src/main/java/cn/zhuhongqing/genericUtils/HelloGit.java
+++ src/main/java/cn/zhuhongqing/genericUtils/HelloGit.java
@@ -4,7 +4,7 @@ package cn.zhuhongqing.genericUtils;
  * Hello world!
  * 
  */
-public class App {
+public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 	}
